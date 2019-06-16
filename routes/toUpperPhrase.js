80f79ec32b31a5f82107cbda8fc01c884a04c31d
Erick Villalobos Aleman <erickva70@gmail.com>
2019-06-16 18:55:09 -0500

    fix

--- routes/toUpperPhrase.js
+++ routes/toUpperPhrase.js
@@ -4,7 +4,7 @@ const { toUpperPhrase } = require('../controller/upperPhrase');
 router.get('/upperPhrase',(req, res) => {
     res.format({
         'application/json': () => {
-            if(req.query.phrase.length)
+            if(req.query.phrase && req.query.phrase.length > 0)
                 res.status(200).send(toUpperPhrase(req.query.phrase));
             
             res.status(200).send(toUpperPhrase());
