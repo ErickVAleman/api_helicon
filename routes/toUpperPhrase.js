f0bd078426985bbb062d71b88fe068bc5aa767f8
Erick Villalobos Aleman <erickva70@gmail.com>
2019-06-16 17:56:47 -0500

    Fix #3

--- routes/toUpperPhrase.js
+++ routes/toUpperPhrase.js
@@ -3,7 +3,12 @@ const { toUpperPhrase } = require('../controller/upperPhrase');
 
 router.get('/upperPhrase',(req, res) => {
     res.format({
-        'application/json': () => res.status(200).send(toUpperPhrase(req.query.phrase)),
+        'application/json': () => {
+            if(req.query.phrase.length)
+                res.status(200).send(toUpperPhrase(req.query.phrase));
+            
+            res.status(200).send(toUpperPhrase());
+        }
     });
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app/app');
 
-var port = process.env.PORT ? process.env.PORT : 3000;
+var port = process.env.PORT ? process.env.PORT : 5000;
 
 app.listen(port, () => {
     console.log(`Server started in port ${port}`);
