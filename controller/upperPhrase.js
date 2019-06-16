9e02e375ecf537225e575ed312c48038a57e65c0
Erick Villalobos Aleman <erickva70@gmail.com>
2019-06-16 17:50:22 -0500

    fix 2

--- controller/upperPhrase.js
+++ controller/upperPhrase.js
@@ -1,6 +1,7 @@
 module.exports = {
     toUpperPhrase: function (phrase = "Hello") {
         if(phrase == "" || phrase == null ){
+            phrase = "hello";
             return phrase.toUpperCase()
         }
         return {
